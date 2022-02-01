f7a72394629d29877ac9d436961621de83c76a4e
Renter <ebealer@gmail.com>
2022-02-01 10:42:47 -0500

    Initial Commit. Basic app with 2 buttons and imageview

---
+++ app/src/main/java/com/mrrenter/sudokusolver/MainActivity.java
@@ -0,0 +1,58 @@
+package com.mrrenter.sudokusolver;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Button captureImageBtn, solveImageBtn;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initilize aspects
+        captureImageBtn = findViewById(R.id.take_picture_btn);
+        solveImageBtn = findViewById(R.id.solve_btn);
+        imageView = findViewById(R.id.image_view);
+
+        //This is called lambda. No need for all the extra nonsense.
+        captureImageBtn.setOnClickListener(view -> dispatchTakePictureIntent());
+    }
+
+    private void solveImage(){
+
+    }
+
+    //Method to be called to open the camera
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
+    //Method called when an image is taken
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+            solveImage();
+        }
+    }
+}
