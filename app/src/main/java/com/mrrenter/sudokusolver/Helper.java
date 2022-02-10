7a2b486f2f29303dbc3db2dd8569fcb7b65a9dac
Renter <ebealer@gmail.com>
2022-02-10 03:08:07 -0500

    Introduced pretty good classifier for detecting only squares within certain sizes. Need to work on a forumla to remove hardcoded sizes.

--- app/src/main/java/com/mrrenter/sudokusolver/Helper.java
+++ app/src/main/java/com/mrrenter/sudokusolver/Helper.java
@@ -1,6 +1,9 @@
 package com.mrrenter.sudokusolver;
 
+import android.util.Log;
+
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
@@ -17,51 +20,94 @@ import java.util.List;
 public class Helper {
     CameraBridgeViewBase mOpenCvCameraView;
     double areaMinThreshold, areaMaxThreshold;
+    double slopeThresh = 0.5;
 
-
-    public Helper(CameraBridgeViewBase camera){
+    public Helper(CameraBridgeViewBase camera) {
         mOpenCvCameraView = camera;
 
-        areaMinThreshold = mOpenCvCameraView.getWidth()*mOpenCvCameraView.getHeight()*0.60;
-        areaMaxThreshold = mOpenCvCameraView.getWidth()*mOpenCvCameraView.getHeight()*0.80;
+        areaMinThreshold = 200000; //mOpenCvCameraView.getWidth()*mOpenCvCameraView.getHeight()*0.50;
+        areaMaxThreshold = 600000;//mOpenCvCameraView.getWidth() * mOpenCvCameraView.getHeight() * 0.80;
     }
 
-    public Mat prepImage(Mat image){
+    public Mat prepImage(Mat image) {
         Mat modifiedFrame = image;
-        Imgproc.GaussianBlur(image, modifiedFrame, new Size(7,7),3);
+        Imgproc.GaussianBlur(image, modifiedFrame, new Size(7, 7), 3);
         Imgproc.adaptiveThreshold(modifiedFrame, modifiedFrame, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 11, 2);
-        Core.bitwise_not(modifiedFrame,modifiedFrame);
+        Core.bitwise_not(modifiedFrame, modifiedFrame);
         return modifiedFrame;
     }
 
     public Mat getLargestRect(Mat bit, Mat img) {
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
         Imgproc.findContours(bit, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-        double inf = 0;
+        Point[] maxpoint = new Point[0];
         Rect max_rect = null;
-        for(int i=0; i< contours.size();i++){
+        for (int i = 0; i < contours.size(); i++) {
             Rect rect = Imgproc.boundingRect(contours.get(i));
 
             MatOfPoint2f c2f = new MatOfPoint2f(contours.get(i).toArray());
             MatOfPoint2f approx = new MatOfPoint2f();
 
             double peri = Imgproc.arcLength(c2f, true);
-            Imgproc.approxPolyDP(c2f, approx, 0.04*peri, true);
+            Imgproc.approxPolyDP(c2f, approx, 0.04 * peri, true);
             Point[] points = approx.toArray();
+
             if (points.length == 4) {
-                //Not making sure its actually a square
                 double area = rect.area();
-                if (area > areaMinThreshold) {
-                    if (inf < area) {
-                        max_rect = rect;
-                        inf = area;
+
+                if (area > areaMinThreshold && area < areaMaxThreshold) {
+
+                    Point min = getMinPoint(points);
+                    Point max = getMaxPoint(points);
+
+                    double slope = (max.y - min.y) / (max.x - min.x);
+                    if (slope < 0){
+                        slope *= -1;
                     }
+
+                    if (slope > (1 - slopeThresh) && slope < (1 + slopeThresh)) {
+                        Log.d("MrRenterDebug", "Area: " + area + " Slope: " + slope);
+                        max_rect = rect;
+                        maxpoint = points;
+                    }
+
                 }
             }
         }
+
         if (max_rect != null) {
             Imgproc.rectangle(img, new Point(max_rect.x, max_rect.y), new Point(max_rect.x + max_rect.width, max_rect.y + max_rect.height), new Scalar(0, 255, 0), 5);
         }
         return img;
+    }
+
+    public Point getMinPoint(Point[] points) {
+        double min = points[0].x + points[0].y;
+        int minIndex = 0;
+
+        for (int x = 0; x < points.length; x++) {
+            double tmp = points[x].x + points[x].y;
+            if (tmp < min) {
+                minIndex = x;
+                min = tmp;
+            }
+        }
+
+        return points[minIndex];
+    }
+
+    public Point getMaxPoint(Point[] points) {
+        double min = points[0].x + points[0].y;
+        int maxIndex = 0;
+
+        for (int x = 0; x < points.length; x++) {
+            double tmp = points[x].x + points[x].y;
+            if (tmp > min) {
+                maxIndex = x;
+                min = tmp;
+            }
+        }
+
+        return points[maxIndex];
     }
 }
